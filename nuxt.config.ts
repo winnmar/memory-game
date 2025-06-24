495e84b9f6354463f51df1fbebe4301cc25a3db0
Mariusz Winnicki <winnickimariusz96@gmail.com>
2025-06-24 21:10:45 +0200

    added favicon and header title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'CS2 Memory Game',
+      htmlAttrs: {
+        lang: 'en',
+      },
+    },
+  },
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image'],
   devtools: { enabled: true },
   css: [
